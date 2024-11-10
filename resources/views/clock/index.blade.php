28d87d708fe89d5ab083ba5d0eb473ed2f8762b9
Ahmad Saiful Bahri <epool862@gmail.com>
2024-11-10 15:27:43 +0800

    change page title

--- resources/views/clock/index.blade.php
+++ resources/views/clock/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Clock In/Out') }}
+            {{ __('Clock In/Out Page') }}
         </h2>
     </x-slot>
 
